030651d9aa09832edffcc076d3ae9b89c628e85a
Minkyu Yang <parigu2@msn.com>
2018-08-03 13:53:24 -0500

    slug ing

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const adding = require('../views/addPage.js');
+const { Page } = require("../models");
+const { addPage } = require("../views");
+
+router.post('/', async (req, res, next) => {
+
+  // STUDENT ASSIGNMENT:
+  // add definitions for `title` and `content`
+  
+  const page = new Page({
+      title: req.body.title,
+      content: req.body.content,
+      name: req.body.name,
+      email: req.body.email,
+      status: req.body.status,
+      slug: `http://localhost:3000/wiki/${req.body.title}`
+    });
+    
+    // make sure we only redirect *after* our save is complete!
+    // note: `.save` returns a promise.
+    try {
+        await page.save();
+        res.redirect('/');
+    } catch (error) { next(error) }
+});
 
 router.get('/', (req, res, next) => {
     res.send('wiki')
 });
 
-router.post('/', (req, res, next) => {
-  res.json(req.body);
-  res.send('wiki post')
-});
+
 
 router.get('/add', (req, res, next) =>{
     res.send(adding())
--- views/addPage.js
+++ views/addPage.js
@@ -7,16 +7,16 @@ module.exports = () => layout(html`
   <form method="POST" action="/wiki/">
 
   <div class="form-group">
-      <label for="title" class="col-sm-2 control-label">Author Name</label>
+      <label for="name" class="col-sm-2 control-label">Author Name</label>
       <div class="col-sm-10">
-        <input id="title" name="title" type="text" class="form-control"/>
+        <input id="name" name="name" type="text" class="form-control"/>
       </div>
     </div>
 
     <div class="form-group">
-      <label for="title" class="col-sm-2 control-label">Author email</label>
+      <label for="email" class="col-sm-2 control-label">Author email</label>
       <div class="col-sm-10">
-        <input id="title" name="title" type="text" class="form-control"/>
+        <input id="email" name="email" type="text" class="form-control"/>
       </div>
     </div>
 
@@ -28,16 +28,16 @@ module.exports = () => layout(html`
     </div>
 
     <div class="form-group">
-      <label for="title" class="col-sm-2 control-label">Page Content</label>
+      <label for="content" class="col-sm-2 control-label">Page Content</label>
       <div class="col-sm-10">
-        <input id="title" name="title" type="text" class="form-control"/>
+        <input id="content" name="content" type="text" class="form-control"/>
       </div>
     </div>
 
     <div class="form-group">
-      <label for="title" class="col-sm-2 control-label">Page Status</label>
+      <label for="status" class="col-sm-2 control-label">Page Status</label>
       <div class="col-sm-10">
-        <input id="title" name="title" type="text" class="form-control"/>
+        <input id="status" name="status" type="text" class="form-control"/>
       </div>
     </div>
 
