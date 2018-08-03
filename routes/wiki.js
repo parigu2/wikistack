721531a7100ef8b6b01266be1f4f39476615c7d3
Minkyu Yang <parigu2@msn.com>
2018-08-03 15:23:16 -0500

    break time

--- routes/wiki.js
+++ routes/wiki.js
@@ -16,7 +16,7 @@ router.post('/', async (req, res, next) => {
     try {
         await page.save();
         console.log(page);
-        res.redirect('/');
+        res.redirect(`/wiki/${page.slug}`);
     } catch (error) { next(error) }
 });
 
@@ -27,8 +27,22 @@ router.get('/', (req, res, next) => {
 
 router.get('/add', (req, res, next) =>{
     res.send(adding())
-})
+});
 
+
+router.get('/:slug', async (req, res, next) => {
+    try {
+        const page = await Page.findOne({
+            where: {
+                slug: req.params.slug
+            }
+        });
+        res.json(page);
+    } catch (error) {
+        next(error)
+    }
+    res.send(`hit dynamic route at ${req.params.slug}`);
+});
 
 
 
