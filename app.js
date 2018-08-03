ef14e1b79f82c87726a45c235b3dac4625116d0c
Minkyu Yang <parigu2@msn.com>
2018-08-03 11:49:06 -0500

    adding pages

--- app.js
+++ app.js
@@ -7,16 +7,15 @@ const models = require('./models');
 const wiki = require('./routes/wiki');
 const user = require('./routes/user');
 
-
 // db.authenticate().
 // then(() => {
 //   console.log('connected to the database');
 // })
 
-
-
 app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({ extended: false }))
+app.use('/wiki', wiki);
+app.use('/user',user);
 
 app.use(morgan('dev'));
 
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const adding = require('../views/addPage.js');
+
+router.get('/', (req, res, next) => {
+    res.send('wiki')
+});
+
+router.post('/', (req, res, next) => {
+    res.send('wiki post')
+});
+
+router.get('/add', (req, res, next) =>{
+    res.send(adding())
+})
 
 
 
